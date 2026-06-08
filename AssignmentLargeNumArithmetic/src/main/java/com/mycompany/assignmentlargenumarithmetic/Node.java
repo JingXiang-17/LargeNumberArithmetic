b1812843e3a7bf7a90e0e76a7c604e8d654d701b
Kor Jing Xiang <25005896@siswa.um.edu.my>
2026-06-08 21:56:04 +0800

    Enhance Node class with documentation comments
    Added comments to explain the Node class and its functionality.

--- AssignmentLargeNumArithmetic/src/main/java/com/mycompany/assignmentlargenumarithmetic/Node.java
+++ AssignmentLargeNumArithmetic/src/main/java/com/mycompany/assignmentlargenumarithmetic/Node.java
@@ -1,17 +1,25 @@
 package com.mycompany.assignmentlargenumarithmetic;
 
+/* Node class
+Every digit of the large number input will be stored as node, and the properties are defined in this class.
+The nodes are designed to create a doubly linked list so that bidirectional traversal is possible.
+*/
+
 public class Node {
 
+    //instance variables
     private int element;
     private Node prev;
     private Node next;
 
+    //Parameterized Node constructor that takes a single digit (int type) as argument
     public Node(int element) {
         this.element = element;
         this.prev = null;
         this.next = null;
     }
 
+    //getters and setters
     public int getElement() {
         return element;
     }
